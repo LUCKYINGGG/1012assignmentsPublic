1d382d958da204ce5daca5ac7303e8d372f4482d
LUCKYING <39611265+LUCKYINGGG@users.noreply.github.com>
2024-04-09 16:47:08 -0600

    a4 class

---
+++ a4/client.cs
@@ -0,0 +1,62 @@
+
+
+namespace Client
+{
+    public class Client
+    {
+        private string _firstname;
+        private string _lastname;
+        private int _weight;
+        private int _height;
+
+//greedy constructor
+        public Client(string firstname, string lastname, int weight, int height)
+        {
+            _firstname = firstname;
+            _lastname = lastname;
+            _weight = weight;
+            _height = height;
+        }
+
+//Properties
+        public string Firstname
+        {
+            get { return _firstname; }
+            set { _firstname = value; }
+        }
+        public string Lastname
+        {
+            get { return _lastname; }
+        }
+        
+        public int Weight{
+            get { return _weight; }
+        }
+
+        public int Height{
+            get { return _height; }
+        }
+        
+        public double BmiScore
+        {
+            get { 
+                double bmi = _weight / _height * _height * 703;
+                return bmi;
+                }
+        }
+        public string BmiStatus
+        {
+            get { 
+                return "status";
+            }
+        }
+
+        public string FullName
+        {
+            get { return $"{_lastname}, {_firstname}"; }
+        }
+        
+
+    }// end of class
+
+}// end of namespace
